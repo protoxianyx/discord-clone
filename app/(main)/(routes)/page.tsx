7eec199dc72a9021e7e7ff8e43ac995a86f7262c
Xianyx <pushkar44320@gmail.com>
2024-07-21 11:28:44 +0530

    basic auth setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,7 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <div className="flex flex-col">This is a protected route</div>;
+  return <div className="flex flex-col">
+    <UserButton/>
+  </div>;
 }
