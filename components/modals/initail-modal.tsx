db7ee672b8fed0dbc60c13d0de71e07d6b09cb91
Xianyx <pushkar44320@gmail.com>
2024-08-02 21:24:26 +0530

    Setup Server Modal

--- components/modals/initail-modal.tsx
+++ components/modals/initail-modal.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+
+
+import axios from "axios";
 import { DialogDescription } from "@radix-ui/react-dialog";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -26,6 +29,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FileUpload } from "@/components/file-upload";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -38,6 +42,8 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
   const [isMounted, setIsMounted] = useState(false);
+
+  const router = useRouter();
 
   useEffect(() => {
     setIsMounted(true);
@@ -54,7 +60,16 @@ export const InitialModal = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      await axios.post("/api/servers", values);
+
+      form.reset();
+      router.refresh();
+      window.location.reload();
+
+    } catch (error) {
+      console.log(error)
+    }
   };
 
   if (!isMounted) {
